c25a231172857ab4dddffebc15fadcb4a71d13c8
bonobo <arburpee@gmail.com>
2022-03-30 23:28:10 -0700

    authentication

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -4,18 +4,6 @@ const { signToken } = require('../utils/auth');
 
 const resolvers = {
   Query: {
-    me: async (parent, args, context) => {
-      if (context.user) {
-        const userData = await User.findOne({ _id: context.user._id })
-          .select('-__v -password')
-          .populate('thoughts')
-          .populate('friends');
-
-        return userData;
-      }
-
-      throw new AuthenticationError('Not logged in');
-    },
     users: async () => {
       return User.find()
         .select('-__v -password')
